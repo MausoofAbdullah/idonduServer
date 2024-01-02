8e75e05cb99ed51718e6d8cd4d959ef3b43a5582
MausoofAbdullah <mausoofabdullah@gmail.com>
2024-01-02 17:48:12 +0530

    reach helmet removed

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ import mongoose from "mongoose";
 import multer from "multer"
 import dotenv from "dotenv";
 
-import React from "react"
-import ReactDOMServer from "react-dom/server"
-import {HelmetProvider} from 'react-helmet-async'
-import {StaticRouter} from "react-router-dom"
 
-import App from './client/src/App'
 
 
 
@@ -19,45 +14,6 @@ import cors from "cors"
 import userRoute from "./Routes/userRoute.js"
 
 const app = express();
-
-
-app.use(express.static('build')); // Assuming your client-side build is in the 'build' folder
-
-app.get('*', (req, res) => {
-  const context = {};
-  const helmetContext = {};
-
-  const appMarkup = ReactDOMServer.renderToString(
-    <HelmetProvider context={helmetContext}>
-      <StaticRouter location={req.url} context={context}>
-        <App />
-      </StaticRouter>
-    </HelmetProvider>
-  );
-
-  const { helmet } = helmetContext;
-
-  // Use helmet.title, helmet.meta, helmet.link, etc., in your HTML template
-
-  const html = `
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        ${helmet.title.toString()}
-        ${helmet.meta.toString()}
-        ${helmet.link.toString()}
-        <!-- Add other head elements as needed -->
-      </head>
-      <body>
-        <div id="root">${appMarkup}</div>
-        <!-- Include client-side scripts if needed -->
-      </body>
-    </html>
-  `;
-
-  res.status(200).send(html);
-});
-
 app.use(express.static("uploads"))
 app.use('/images',express.static("images"))
 
